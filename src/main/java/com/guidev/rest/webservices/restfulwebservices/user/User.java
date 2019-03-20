51c9f3b63f28adb2d4b930e89317085940f50d84
Guilherme Alencar A <9uilhermealencar@gmail.com>
2019-03-20 18:33:19 -0300

    User Validation

--- src/main/java/com/guidev/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/guidev/rest/webservices/restfulwebservices/user/User.java
@@ -1,13 +1,17 @@
 package com.guidev.rest.webservices.restfulwebservices.user;
 
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 public class User {
 
     private Integer id;
 
+    @Size(min=2, message = "Name has to be bigger than 1 character")
     private String name;
 
+    @Past
     private Date birthDate;
 
     protected User(){}
